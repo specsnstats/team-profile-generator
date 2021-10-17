b0ea53ddc6b5ccde869706938d758b0b5050baec
SpecsNStats <specsnstats@gmail.com>
2021-10-16 19:44:23 -0600

    actually read the readme, arranged the order of the inquirer prompts to match the requirements. also added the populateTeam

--- index.js
+++ index.js
@@ -1,62 +1,142 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const Employee = require("./lib/Employee")
 const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
 const Engineer = require("./lib/Engineer");
-const { log } = require("console");
+const generateHTML = require("./util/generateHtml");
 
-inquirer.prompt([
-    {
-        type: 'input',
-        message: 'Employee Name:',
-        name: 'name',
-    },
-    {
-        type: 'input',
-        message: 'ID Number:',
-        name: "id"
-    },
-    {
-        type: "input",
-        message: "Employee Email:",
-        name: "email"
-    },
-    {
-        type: "list",
-        message: "Role:",
-        name: "role",
-        choices: [
-            { name: "Manager" },
-            { name: "Engineer" },
-            { name: "Intern" },
-        ]
-    },
-    {
-        type: "input",
-        message: "Manager Office Number:",
-        name: "officeNumber",
-        when: function(response) {
-            return response.role == "Manager"
-        }
-    },
-    {
-        type: "input",
-        message: "GitHub Username:",
-        name: "github",
-        when: function(response) {
-            return response.role == "Engineer"
-        }
-    },
-    {
-        type: "input",
-        message: "School:",
-        name: "school",
-        when: function(response) {
-            return response.role == "Intern"
-        }
-    }
-])
-    .then(({ name, email, role, school, officeNumber, github }) => {
-        console.log(name, email, role, officeNumber, github, school)
+team = []
 
+const addManager = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Manager Name:',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'ID Number:',
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Manager Email:",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "Manager Office Number:",
+            name: "officeNumber",
+        }
+    ])
+    .then(({name, id, email, officeNumber}) => {
+        const myManager = new Manager(name, email, id, officeNumber)
+        team.push(myManager)
+        addOptions()
     })
+    .catch((err) => {
+        console.log(err)
+    })
+}
+
+const addEngineer = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Engineer Name:',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'ID Number:',
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Engineer Email:",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "Engineer GitHub:",
+            name: "github",
+        }
+    ])
+    .then(({name, id, email, github}) => {
+        const myEngineer = new Engineer(name, email, id, github)
+        team.push(myEngineer)
+        addOptions()
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+}
+
+const addIntern = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Intern Name:',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'ID Number:',
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Intern Email:",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "School:",
+            name: "school",
+        }
+    ])
+    .then(({name, id, email, school}) => {
+        const myIntern = new Intern(name, email, id, school)
+        team.push(myIntern)
+        addOptions()
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+}
+
+function addOptions() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What Next?",
+            name: "addAnother",
+            choices: [
+                { name: "Add an Engineer" },
+                { name: "Add an Intern" },
+                { name: "Finish Building my Team" }
+            ]
+        }
+    ])
+    .then(({ addAnother }) => {
+        switch (addAnother){
+            case "Add an Engineer":
+                addEngineer()
+                break;
+            case "Add an Intern":
+                addIntern()
+                break;
+            case "Finish Building my Team":
+                populateTeam()
+        }
+    })
+}
+
+const populateTeam = () => {
+    fs.writeFile("./index.html", generateHTML(team), (err) =>
+    err? console.log(err) : console.log('Team Populated!'))
+}
+
+addManager()
